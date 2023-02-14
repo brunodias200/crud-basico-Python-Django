b17983e6a6c5dadda32a7edaeb24f58e142ffa7b
Bruno Dias <brunoadias1996@gmail.com>
2023-02-14 08:11:40 -0300

    fix: corrigindo arquivo .gitignore

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from app.models import Carros
-
-class CarrosForm(ModelForm):
-    class Meta:
-        model = Carros
-        fields = ['modelo', 'marca', 'ano']
