ef19f6e6d8c2c1119c8297c14be74539ebb389f8
Bruno Dias <brunoadias1996@gmail.com>
2023-02-14 08:08:06 -0300

    feat: created update and delete

---
+++ app/static/javascript.js
@@ -0,0 +1,18 @@
+(function(win, doc){
+    'use strict'
+
+    //confirma deleção
+    // if(doc.querySelector('.btnDel')){
+    //     // let btnDel = doc.querySelectorAll('.btnDel')
+    //     // for(i in btnDel){
+    //     //     btnDel[i].addEventListener('click', function(event){
+    //     //         if(confirm('deseja mesmo apagar esse dado')){
+    //     //             return true
+    //     //         }
+    //     //         else{
+    //     //             event.preventDefault()
+    //     //         }
+    //     //     })
+    //     // }
+    // }
+})(window,document)
